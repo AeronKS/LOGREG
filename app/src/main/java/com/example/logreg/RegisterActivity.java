1ab6b139a0a8f31cd93ec32ad940ad6f17894387
Szucs Bence <bence.szucs3513@gmail.com>
2020-12-13 20:23:30 +0100

    Regisztrációs adatok ellenőrzése és bevitele az adatbázisba

--- app/src/main/java/com/example/logreg/RegisterActivity.java
+++ app/src/main/java/com/example/logreg/RegisterActivity.java
@@ -7,11 +7,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
 
     EditText etEmail, etFelhnev, etJelszo, etTeljesNev;
     Button btnReg, btnVissza;
+    DBhelper adatbazis;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +41,34 @@ public class RegisterActivity extends AppCompatActivity {
 
     private void RegAdatRogzit() {
         //todo adatrögzítés
+        String email = etEmail.getText().toString().trim();
+        String felhnev = etFelhnev.getText().toString().trim();
+        String jelszo = etJelszo.getText().toString().trim();
+        String teljesnev = etTeljesNev.getText().toString().trim();
+
+        if (email.isEmpty()) {
+            Toast.makeText(this, "E-mail megadása kötelező!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (felhnev.isEmpty()) {
+            Toast.makeText(this, "Felhasználónév megadása kötelező!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (jelszo.isEmpty()) {
+            Toast.makeText(this, "Jelszó megadása kötelező!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (teljesnev.isEmpty()) {
+            Toast.makeText(this, "Név megadása kötelező!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (adatbazis.adatRegisztracio(email, felhnev, jelszo, teljesnev)){
+            Toast.makeText(this, "Sikeres regisztráció!", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Sikertelen regisztráció!", Toast.LENGTH_SHORT).show();
+        }
+
     }
 
     private void init() {
@@ -48,5 +78,7 @@ public class RegisterActivity extends AppCompatActivity {
         etTeljesNev = findViewById(R.id.et_reg_teljnev);
         btnReg = findViewById(R.id.btn_reg_regisztracio);
         btnVissza = findViewById(R.id.btn_reg_vissza);
+
+        adatbazis = new DBhelper(RegisterActivity.this);
     }
 }
